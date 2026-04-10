ae62cb6015874a2072f9dc151e20db8410caa0ba
Aditya-00001 <adityanjena1@gmail.com>
2026-04-10 10:13:01 +0530

    smooth scrolling to top

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,18 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
 import './App.css'
 import Nav from './components/nav.jsx'
 import Footer from './components/footer.jsx'
 import { BrowserRouter } from 'react-router-dom';
-import AdminDashboard from './components/AdminDashboard.jsx';
-import ProtectedRoute from './components/ProtectedRoute';
-function App() {
-  // const [count, setCount] = useState(0)
+import ScrollToTop from './components/ScrollToTop.jsx';
 
+function App() {
   return (
     <>
-    <BrowserRouter>
-      <Nav />
-      {/* Your main page content will render here via the Routes in Nav */}
-      <Footer />
-      {/* <AdminDashboard /> */}
-    </BrowserRouter>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Nav />
+        {/* Main page content renders via the Routes in Nav */}
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
